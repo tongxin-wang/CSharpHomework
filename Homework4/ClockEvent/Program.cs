84478e78635e38cc06653389e72e060f91c8c45d
Tongxin Wong <dunkywong@gmail.com>
2020-03-13 11:53:54 +0800

    C# homework for the fourth week

---
+++ Homework4/ClockEvent/Program.cs
@@ -0,0 +1,93 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Threading;
+
+namespace ClockEvent
+{
+
+    //声明一个委托类型，定义事件处理函数的格式 下面两个时间处理函数类似
+    public delegate void TickHandler(object sender, TickEventArgs args);
+    public class TickEventArgs
+    {
+        public DateTime CurrentTime { get; }
+        public TickEventArgs(DateTime time)
+        {
+            CurrentTime = time;
+        }
+    }
+
+    public delegate void AlarmHandler(object sender, AlarmEventArgs args);
+    public class AlarmEventArgs
+    {
+        public DateTime AlarmTime { get; }
+        public AlarmEventArgs(DateTime time)
+        {
+            AlarmTime = time;
+        }
+    }
+
+    public class AlarmClock
+    {
+        //闹钟响铃时间
+        private DateTime alarmTime;
+        //定义事件，相当于创建一个委托实例
+        public event TickHandler OnTick;
+        public event AlarmHandler OnAlarm;
+
+        //Run方法每隔一秒触发一次OnTick事件，并且时间到了设定的闹铃时间会触发OnAlarm事件响铃
+        public void Run()
+        {
+            alarmTime = System.DateTime.Now.AddSeconds(10);
+            Console.WriteLine($"The alarm clock will go off in ten seconds({alarmTime.ToString("HH:mm:ss")}).");
+            DateTime tmpTime;
+
+            while(DateTime.Compare(alarmTime, tmpTime = System.DateTime.Now) > 0)
+            {
+                TickEventArgs args = new TickEventArgs(tmpTime);
+                //触发onTick事件
+                OnTick(this, args);
+                Thread.Sleep(1000);
+            }
+
+            AlarmEventArgs args1 = new AlarmEventArgs(tmpTime);
+            OnAlarm(this, args1);
+            return;
+        }
+    }
+
+    public class Form
+    {
+        public AlarmClock alarmClock = new AlarmClock();
+
+        public Form()
+        {
+            //为AlarmClock的OnTick事件添加处理方法
+            alarmClock.OnTick += new TickHandler(Clock_OnTick);
+            //为AlarmClock的OnAlarm事件添加处理方法
+            alarmClock.OnAlarm += new AlarmHandler(Clock_OnAlarm);
+        }
+
+        void Clock_OnTick(object sender, TickEventArgs args)
+        {
+            Console.WriteLine($"Tick! Now time is {args.CurrentTime.ToString("HH:mm:ss")}");
+        }
+
+        void Clock_OnAlarm(object sender, AlarmEventArgs args)
+        {
+            Console.WriteLine($"Ring! Time's up! Now time is {args.AlarmTime.ToString("HH:mm:ss")}");
+        }
+    }
+
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Form form1 = new Form();
+            form1.alarmClock.Run();
+        }
+    }
+}
---
+++ Homework4/GenericApplication/Program.cs
@@ -0,0 +1,105 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace GenericApplication
+{
+
+    // 链表节点
+    public class Node<T>
+    {
+        public Node<T> Next { get; set; }
+        public T Data { get; set; }
+
+        public Node(T t)
+        {
+            Next = null;
+            Data = t;
+        }
+    }
+
+    //泛型链表类
+    public class GenericList<T>
+    {
+        private Node<T> head;
+        private Node<T> tail;
+
+        public GenericList()
+        {
+            tail = head = null;
+        }
+
+        public Node<T> Head
+        {
+            get => head;
+        }
+
+        public void Add(T t)
+        {
+            Node<T> n = new Node<T>(t);
+            if(tail == null)
+            {
+                head = tail = n;
+            }
+            else
+            {
+                tail.Next = n;
+                tail = n;
+            }
+        }
+
+        public void ForEach(Action<T> action)
+        {
+            Node<T> tmp = head;
+            while(tmp != null)
+            {
+                action(tmp.Data);
+                tmp = tmp.Next;
+            }
+        }
+    }
+
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            // 整型List
+            GenericList<int> intlist = new GenericList<int>();
+            for(int x = 0; x < 10; x++)
+            {
+                intlist.Add(x);
+            }
+            //for(Node<int> node = intlist.Head;
+            //      node != null; node = node.Next)
+            //{
+            //    Console.WriteLine(node.Data);
+            //}
+            int sum = 0;
+            int max = Int32.MinValue;
+            int min = Int32.MaxValue;
+            intlist.ForEach(x => Console.WriteLine(x));
+            intlist.ForEach(x => { if(x > max) max = x; });
+            intlist.ForEach(x => { if(x < min) min = x; });
+            intlist.ForEach(x => sum += x);
+            Console.WriteLine($"max = {max}");
+            Console.WriteLine($"min = {min}");
+            Console.WriteLine($"sum = {sum}");
+
+            // 字符串型List
+            GenericList<string> strList = new GenericList<string>();
+            for(int x = 0; x < 10; x++)
+            {
+                strList.Add("str" + x);
+            }
+            //for(Node<string> node = strList.Head;
+            //        node != null; node = node.Next)
+            //{
+            //    Console.WriteLine(node.Data);
+            //}
+            strList.ForEach(x => Console.WriteLine(x));
+
+        }
+    }
+}
