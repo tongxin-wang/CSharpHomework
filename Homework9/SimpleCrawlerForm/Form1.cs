c125fea480f060a05e302d9dc2e52076e25ff06b
Tongxin Wong <dunkywong@gmail.com>
2020-05-30 14:55:24 +0800

    C# homework for the ninth week

---
+++ Homework9/SimpleCrawlerForm/Form1.cs
@@ -0,0 +1,76 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Text.RegularExpressions;
+using System.Threading;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace SimpleCrawlerForm
+{
+    public partial class Form1 : Form
+    {
+        public List<DownloadInfo> DownloadInfos { get; set; }
+        SimpleCrawler simpleCrawler = new SimpleCrawler();
+        Thread thread = null;   //如果不用线程会卡顿
+
+        public Form1()
+        {
+            InitializeComponent();
+            DownloadInfos = new List<DownloadInfo>();
+            infoBindingSource.DataSource = DownloadInfos;
+            simpleCrawler.CrawlerStopped += SimpleCrawler_CrawlerStopped;
+            simpleCrawler.DownloadCompleted += SimpleCrawler_DownloadCompleted;
+        }
+
+        private void SimpleCrawler_DownloadCompleted(string url, string info)
+        {
+            DownloadInfos.Add(new DownloadInfo { Id = DownloadInfos.Count + 1, Url = url, Status = info });
+            Action action = () => infoBindingSource.ResetBindings(false);
+            if(this.InvokeRequired)
+            {
+                this.Invoke(action);
+            }
+            else
+            {
+                action();
+            }
+        }
+
+        private void SimpleCrawler_CrawlerStopped()
+        {
+            Action action = () => MessageBox.Show("爬行结束！");
+            if(this.InvokeRequired)
+            {
+                this.Invoke(action);
+            }
+            else
+            {
+                action();
+            }
+        }
+
+        private void btnStart_Click(object sender, EventArgs e)
+        {
+            DownloadInfos.Clear();
+            simpleCrawler.StartURL = txtUrl.Text;
+
+            Match match = Regex.Match(simpleCrawler.StartURL, SimpleCrawler.urlParseRegex);
+            if(match.Length == 0) return;
+            string host = match.Groups["host"].Value;
+            simpleCrawler.HostFilter = "^" + host + "$";
+            simpleCrawler.FileFilter = ".html?$";
+
+            if(thread != null)
+            {
+                thread.Abort();
+            }
+            thread = new Thread(simpleCrawler.Crawl);
+            thread.Start();
+        }
+    }
+}
---
+++ Homework9/SimpleCrawlerForm/SimpleCrawler.cs
@@ -0,0 +1,132 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Net;
+using System.Text;
+using System.Text.RegularExpressions;
+using System.Threading;
+using System.Threading.Tasks;
+
+namespace SimpleCrawlerForm
+{
+    public class SimpleCrawler
+    {
+        public event Action CrawlerStopped;
+        public event Action<string, string> DownloadCompleted;
+
+        //所有已下载和待下载URL
+        private Hashtable urls = new Hashtable();
+
+        //待下载队列
+        private Queue<string> pending = new Queue<string>();
+
+        //URL检测表达式，用于在HTML文本中查找URL
+        private readonly string urlDetectRegex = @"(href|HREF)\s*=\s*[""'](?<url>[^""'#>]+)[""']";
+
+        //URL解析表达式
+        public static readonly string urlParseRegex = @"^(?<site>https?://(?<host>[\w\d.]+)(:\d+)?($|/))([\w\d]+/)*(?<file>[^#?]*)";
+        public string HostFilter { get; set; } //主机过滤规则
+        public string FileFilter { get; set; } //文件过滤规则
+        public int MaxPage { get; set; } //最大下载数量
+        public string StartURL { get; set; } //起始网址
+
+        public SimpleCrawler()
+        {
+            MaxPage = 100;
+        }
+
+        public void Crawl()
+        {
+            urls.Clear();
+            pending.Clear();
+            pending.Enqueue(StartURL);
+
+            while(urls.Count < MaxPage && pending.Count > 0)
+            {
+                string url = pending.Dequeue();
+                try
+                {
+                    string html = DownLoad(url);
+                    urls[url] = true;
+                    DownloadCompleted(url, "成功");
+                    Parse(html, url); //解析，并加入新的链接
+                }
+                catch(Exception ex)
+                {
+                    DownloadCompleted(url, "  失败:" + ex.Message);
+                }
+            }
+            CrawlerStopped();
+        }
+
+        public string DownLoad(string url)
+        {
+            WebClient webClient = new WebClient();
+            webClient.Encoding = Encoding.UTF8;
+            string html = webClient.DownloadString(url);
+            string fileName = urls.Count.ToString();
+            File.WriteAllText(fileName, html, Encoding.UTF8);
+            return html;
+        }
+
+        private void Parse(string html, string pageUrl)
+        {
+            var matches = new Regex(urlDetectRegex).Matches(html);
+            foreach(Match match in matches)
+            {
+                string linkUrl = match.Groups["url"].Value;
+                if(linkUrl == null || linkUrl == "") continue;
+                linkUrl = FixUrl(linkUrl, pageUrl);//转绝对路径
+
+                //解析出host和file两个部分，进行过滤
+                Match linkUrlMatch = Regex.Match(linkUrl, urlParseRegex);
+                string host = linkUrlMatch.Groups["host"].Value;
+                string file = linkUrlMatch.Groups["file"].Value;
+                if(file == "") file = "index.html";
+
+                if(Regex.IsMatch(host, HostFilter) && Regex.IsMatch(file, FileFilter)
+                  && !urls.ContainsKey(linkUrl))
+                {
+                    pending.Enqueue(linkUrl);
+                    urls.Add(linkUrl, false);
+                }
+            }
+        }
+
+        //将相对路径转为绝对路径
+        static private string FixUrl(string url, string baseUrl)
+        {
+            if(url.Contains("://"))
+            {
+                return url;
+            }
+            if(url.StartsWith("//"))
+            {
+                return "http:" + url;
+            }
+            if(url.StartsWith("/"))
+            {
+                Match urlMatch = Regex.Match(baseUrl, urlParseRegex);
+                String site = urlMatch.Groups["site"].Value;
+                return site.EndsWith("/") ? site + url.Substring(1) : site + url;
+            }
+
+            if(url.StartsWith("../"))
+            {
+                url = url.Substring(3);
+                int idx = baseUrl.LastIndexOf('/');
+                return FixUrl(url, baseUrl.Substring(0, idx));
+            }
+
+            if(url.StartsWith("./"))
+            {
+                return FixUrl(url.Substring(2), baseUrl);
+            }
+
+            int end = baseUrl.LastIndexOf("/");
+            return baseUrl.Substring(0, end) + "/" + url;
+        }
+    }
+}
